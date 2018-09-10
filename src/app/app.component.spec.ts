4bb3bb72294aafbf1974a45746b3806bb7a76af6
Grey Delamar <grey.delamar@gmail.com>
2018-09-09 20:15:24 -0400

    Inital commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'crypt-watcher'`, async(() => {
+  it(`should have as title 'Crypt-Watcher'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('crypt-watcher');
+    expect(app.title).toEqual('Crypt-Watcher');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to crypt-watcher!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Crypt-Watcher!');
   }));
 });
